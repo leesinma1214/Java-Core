3d7634b1df9cbdc89b3c8d367720cf872a3102b6
leesinma1214 <90822984+leesinma1214@users.noreply.github.com>
2022-10-25 10:53:44 +0700

    Update bai9.java

--- Lesson1/bai9.java
+++ Lesson1/bai9.java
@@ -2,10 +2,10 @@ public class bai9 {
     public static void main(String[]args) {
         System.out.println("Cho 2 so nguyen duong: 1 va 9");
 
-        double a = 1;
-        double b = 9;
-        double c = a/b;
+        int a = 1;
+        int b = 9;
+        double c = (double)a/b;
 
-        System.out.println("Can bac hai cua so nguyen duong da duoc lam tron la: " + ((double) Math.round(c * 1000) / 1000));
+        System.out.println("Ket qua: " + ((double) Math.round(c * 1000) / 1000));
     }
 }
